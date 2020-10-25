475a227b20e2ddfd54df13d3b1ccd207d9145d59
mateus leao <mateuslea81@gmail.com>
2020-10-25 01:24:10 -0600

    db migration done, bootstrap already integrated

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,32 @@
 class HomeController < ApplicationController
   def index
+  #using a variable here to start the stocks api
+  @api = StockQuote::Stock.new(api_key: "pk_c10395aac3814b5697d5d82713e2fe91") 
+  
+  # getting info from the form (coming as a parameter, and using it below), named as ticker in index.html.erb
+  #error handling below
+  if params[:ticker] == ""
+  	@nothing = "You forgot to enter the stock symbol/code"
+	  elsif params[:ticker] #if it exists
+	  	begin # This beging was needed with the rescue because if 
+	  		# if a wrong stock was typed, it would crash the app
+	  		# the rescue avoid the app from crashing
+		  	@stock = StockQuote::Stock.quote(params[:ticker])
+			rescue
+	  		#puts "um, that makes no sense"
+	  		@error = "You typed a wrong stock symbol/code, please try again!"
+		end
+		
+	  	#this is gonna try to render something
+	  	#if !@stock #if stock doesn't exist, then go to line below
+	  		#@error = "The stock symbol/code you input doesn't exist, try again"
+	  	#end
+	  #but we haven't sent anything to the form yet, so we need ERROR_HANDLING WITH THE IF
+	  end
   end
+
+  def about
+  end
+
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
   #get 'home/index'
   root 'home#index'
+  get 'home/about'
+
+  post "/" => 'home#index' #this is pointing the point to the controller, #our lookup will
+  # our lookup will direct to the home#index
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
